8cc27322e7fb853f5ba0345205a219091482b112
Phuntsho Wangdi <phuntshoowangdi@hotmail.com>
2025-08-12 08:40:37 +0600

    add testimonial

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -6,5 +6,11 @@ export const testimonials: ITestimonial[] = [
         role: 'Guests from Singapore',
         message: `Thanks to our tour leader Chhogyal, we had an unforgettable Bhutan experience. From breathtaking alpine treks and scenic valleys to flavorful local food and rich cultural insights — every moment was meaningful. We still talk about it today!`,
         avatar: '/images/Tan Wee Lim Charlie.JPG',
+    },
+    {
+        name: "JeyAr Amsid  ",
+        role: "",
+        message:`My 2018 trip to Bhutan was unforgettable, thanks to Himalayan Horizon Adventures. Their seamless planning, deep cultural insights, and our amazing guide Chhogyal made the journey truly special. Highly recommended for an authentic Bhutan experience!`,
+        avatar: "/images/JeyAr.jpg"
     }
 ];
