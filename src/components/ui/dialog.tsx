a5b7ac28a9d4a2aa360d89e46f93fd627510f1a1
Phuntsho Wangdi <phuntshoowangdi@hotmail.com>
2025-08-18 22:11:42 +0600

    add close text

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -44,9 +44,13 @@ const DialogContent = React.forwardRef<
       {...props}
     >
       {children}
-      <DialogPrimitive.Close className="absolute top-4 left-1/2 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
+      <DialogPrimitive.Close className="absolute top-2 left-1/2 flex items-center gap-2 rounded-sm px-2 
+             opacity-70 ring-offset-background transition-opacity hover:opacity-100 
+             focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 
+             disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
+      >
         <X className="h-4 w-4" />
-        <span className="sr-only">Close</span>
+        <span>Close</span>
       </DialogPrimitive.Close>
     </DialogPrimitive.Content>
   </DialogPortal>
